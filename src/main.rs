d918976075da707fe70fa7c5046a8e16667bd9f2
upitroma <55750919+upitroma@users.noreply.github.com>
2023-02-18 15:23:51 -0500

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+/// this is documentation
+// this is a comment
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    // let mut guess = input();
+    
+    println!("You guessed: {}", input());
+
+}
+
+fn input() -> String{
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    return input;
 }
